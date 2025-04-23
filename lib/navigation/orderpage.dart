e8f07032cec0e8ce5261cb23eccdd405f025ad2e
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-04-23 10:07:06 +0700

    ADD - Orderpage with input controller & price calculation

---
+++ lib/navigation/orderpage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Orderpage extends StatefulWidget {
+  const Orderpage({super.key});
+
+  @override
+  State<Orderpage> createState() => _OrderpageState();
+}
+
+class _OrderpageState extends State<Orderpage> {
+  final TextEditingController makananController = TextEditingController();
+  final TextEditingController minumanController = TextEditingController();
+  final TextEditingController jumlahMakananController = TextEditingController();
+  final TextEditingController jumlahMinumanController = TextEditingController();
+  int totalHarga = 0;
+
+  void calculateTotalPrice() {
+    int jumlahMakanan = int.tryParse(jumlahMakananController.text) ?? 0;
+    int jumlahMinuman = int.tryParse(jumlahMinumanController.text) ?? 0;
+
+    setState(() {
+      totalHarga = (jumlahMinuman * 5000) + (jumlahMakanan * 20000);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
