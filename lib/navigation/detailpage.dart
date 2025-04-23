05e1c1c3d9f9dca7a59609ac75939795d557652b
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-04-23 10:27:04 +0700

    ADD - orderdetail screen

---
+++ lib/navigation/detailpage.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class DetaiOrderPage extends StatelessWidget {
+  final String makanan;
+  final String jumlahMakanan;
+  final String minunan;
+  final String jumlahMinuman;
+  final int totalHarga;
+  const DetaiOrderPage({
+    super.key,
+    required this.makanan,
+    required this.jumlahMakanan,
+    required this.minunan,
+    required this.jumlahMinuman,
+    required this.totalHarga,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Detail Order Page')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Food Order : $makanan'),
+            Text('Food QTY Order : $jumlahMakanan'),
+            Text('Drink Order : $minunan'),
+            Text('Drink QTY Order : $jumlahMinuman'),
+            Text('Total Price : $totalHarga'),
+
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
